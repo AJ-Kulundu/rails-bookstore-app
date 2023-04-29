5cc69e62c15988b8ee1b1342540e6e40dcdc7401
AJ-Kulundu <jameskulundu@gmail.com>
2023-04-29 21:01:12 +0300

    enumerated roles in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #enums
+  enum role: {user: 0, admin: 1}
+
+  
 end
