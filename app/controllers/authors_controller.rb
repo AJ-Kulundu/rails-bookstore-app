08607741f215a4ff4ef9df00901e2a6e9cfd92e4
AJ-Kulundu <jameskulundu@gmail.com>
2023-04-30 11:57:51 +0300

    Adding admin only pages to the authors resource

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,9 +1,21 @@
 class AuthorsController < ApplicationController
     skip_before_action :authenticate_user!, only: [:index,:show]
+    before_action :check_admin, except: [:index,:show]
     def index
         @authors = Author.all
     end
     def show
         @author = Author.find(params[:id])
+    end
+    def new
+        @author = Author.new
+    end 
+
+    private
+
+    def check_admin
+        unless current_user.admin?
+            redirect_to root_path, alert: "You are not authorized to perform this action."
+        end
     end
 end
